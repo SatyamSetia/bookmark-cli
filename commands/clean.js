2bfbf2f603a9b24464133a27f51a485ad4c89ba3
SatyamSetia <satyam7991@gmail.com>
2019-08-05 18:30:34 +0530

    Fix: clean bookmark

--- commands/clean.js
+++ commands/clean.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 function clean() {
-  fs.writeFile(__dirname+'bookmarks.json', JSON.stringify({}), 'utf-8', (err) => {
+  fs.writeFile(__dirname+'\\bookmarks.json', JSON.stringify({}), 'utf-8', (err) => {
     if(err) throw err;
 
     console.log('All bookmarks cleared')
