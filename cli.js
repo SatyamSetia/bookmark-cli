afb8372e3b934146bdc4d0649d72659346f58a36
SatyamSetia <satyam7991@gmail.com>
2019-08-07 14:31:27 +0530

    Update: --help flag

--- cli.js
+++ cli.js
@@ -7,8 +7,16 @@ const cli = meow(`
   Usage
     $ bm [command] [options]
   Options
-    -h, --help
-    -v, --version
+    --help
+    --version
+  Commands
+    add -> Add a new bookmark.
+          NOTE: If you are adding custom path and it contains spaces then enclose it in double quotes.
+    clean -> Delete all existing bookmarks.
+    remove -> Delete a specific bookmark.
+    show -> Show all bookmarks.
+  Example
+    bm mybookmark -> It will open file/folder with bookmark name 'mybookmark'.
   `,{
     alias: {
       h: 'help',
